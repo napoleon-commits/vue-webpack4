c64985186999074a30dfac9e9e03935c949f1cde
Byron Hall <hall@controlpoint.com>
2021-03-17 13:31:48 -0400

    added vuetify

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
+import vuetify from './plugins/vuetify' // path to vuetify export
 
 Vue.component('hello-component', require('./components/HelloComponent').default);
 
 new Vue({
-    el: '#app', 
+    vuetify,
     data: {
         message: 'Hello from Webpack'
     }
-});
+  }).$mount('#app');
