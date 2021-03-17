64eea447a7f2bfdd08ed7bf9a6a1634546a48079
Byron Hall <hall@controlpoint.com>
2021-03-17 15:17:35 -0400

    no corro casi

--- components/HelloComponent.vue
+++ components/HelloComponent.vue
@@ -1,8 +1,19 @@
 <template>
-    <h2 class="text-center">Hello From Component</h2>
+    <div class="container">
+        <div class="row my-5 justify-content-center">
+            <div class="col-sm-6">
+                <h1 class="text-center">{{ message }}</h1>
+                <h2 class="text-center">Hello From Component</h2>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return {
+            message: 'Hello from Webpack',
+        };
+    }
 }
 </script>
