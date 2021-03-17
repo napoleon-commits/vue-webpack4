d6a5cbe9e21c75dbd9cbf6c96f0c7e47f534c077
Byron Hall <hall@controlpoint.com>
2021-03-17 10:25:50 -0400

    added components

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+Vue.component('hello-component', require('./components/HelloComponent').default);
+
 new Vue({
     el: '#app', 
     data: {
---
+++ components/HelloComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <h2 class="text-center">Hello From Component</h2>
+</template>
+<script>
+export default {
+    
+}
+</script>
