4941bc80eb823ab3791f630c071f477e3406f39c
Byron Hall <hall@controlpoint.com>
2021-03-18 11:17:32 -0400

    with vuetify, 48% performance

--- App.vue
+++ App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="container">
-    <div class="row my-5 justify-content-center">
-      <div class="col-sm-6">
-        <h1 class="text-center">{{ message }}</h1>
-        <h2 class="text-center">Hello From Component</h2>
+  <v-app>
+    <div class="container">
+      <div class="row my-5 justify-content-center">
+        <div class="col-sm-6">
+          <h1 class="text-center">{{ message }}</h1>
+          <h2 class="text-center">Hello From Component</h2>
+        </div>
       </div>
     </div>
-  </div>
+  </v-app>
 </template>
 
 <script>
+import {VApp} from 'vuetify/lib';
+
 export default {
+    components: {
+        VApp,
+    },
   data(){
     return {
       message: 'Hello from Webpack',
